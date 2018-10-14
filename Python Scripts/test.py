a7133f1af319ffd04fe6bfda5ac3d3b93453d927
zshaver1@uncc.edu <zshaver1@uncc.edu>
2018-10-14 11:50:17 -0400

    Added quandl test

--- Python Scripts/test.py
+++ Python Scripts/test.py
@@ -1,3 +1,14 @@
-import sys
-print(sys.version)
-import tensorflow
+import quandl
+import pandas as pd
+#wiki continous figures 
+gl = quandl.get("WIKI/GOOGL")
+
+gl = gl[["Adj. Open", "Adj. High", "Adj. Low", "Adj. Close", "Adj. Volume",]]
+
+gl["HL_PCT"] = ( gl["Adj. High"] -  gl["Adj. Close"]) /  gl["Adj. Close"] * 100.0
+
+gl["PCT_change"] = (gl["Adj. Close"] - gl["Adj. Open"]) / gl["Adj. Open"] * 100.0
+
+
+
+print(gl.head())
