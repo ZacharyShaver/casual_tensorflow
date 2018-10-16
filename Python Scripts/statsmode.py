862dad9febf6b7debef3760d9b1aa6ca06a22b20
zshaver1@uncc.edu <zshaver1@uncc.edu>
2018-10-16 14:43:50 -0400

    stats model complete working on linreg

---
+++ Python Scripts/statsmode.py
@@ -0,0 +1,15 @@
+from sklearn import datasets
+import statsmodels.api as sm
+import pandas as pd
+
+b = datasets.load_boston()
+df = pd.DataFrame(b.data, columns=b.feature_names)
+target = pd.DataFrame(b.target, columns=["MEDV"])
+
+x = df[["RM", "LSTAT"]]
+y = target["MEDV"]
+
+
+model = sm.OLS(y, x).fit()
+predictions = model.predict(x)
+print(model.summary()) 
