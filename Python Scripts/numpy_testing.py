ce862549b222a57dc5fef61a19da660f81c33598
zshaver1@uncc.edu <zshaver1@uncc.edu>
2018-10-18 12:02:52 -0400

    Finished numpy example with comments

--- Python Scripts/numpy_testing.py
+++ Python Scripts/numpy_testing.py
@@ -1,15 +1,27 @@
 import numpy as np 
-
+from sklearn.linear_model import LinearRegression
 X = np.array([[1, 1], [1, 2], [2, 2], [2, 3]])
 
 '''
+DOT is dot product !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 col 0 * 1 + col 1 * 2 + 3
-1 1      1 + 2 + 3 =  
-1 2
-2 2 
-2 3
+1 1     1 + 2 + 3 = 6 
+1 2     1 + 4 + 3 = 8    
+2 2     2 + 4 + 3 = 9
+2 3     2 + 6 + 3 = 11
+MATRIX MULIPLICATION
 '''
 y = np.dot(X, np.array([1, 2])) + 3
 
-print(X)
-print(y)
+reg = LinearRegression().fit(X, y)
+
+
+print(reg.score(X, y))
+
+# prints scale array for dot product that was predicted 
+print(reg.coef_)
+# prints the intercept from the y axix 
+print(reg.intercept_)
+
+# gets the numbers for the dot product and intercept and predicts the output for points based off of that 
+print(reg.predict(np.array([[10, 5]])))
