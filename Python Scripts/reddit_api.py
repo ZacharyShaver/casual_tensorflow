a752d42820dcfb52ef6a8c76d7db98ed982fe0a4
Zak Shaver <zshaver1@uncc.edu>
2018-11-06 10:42:26 -0500

    Add reddit api

---
+++ Python Scripts/reddit_api.py
@@ -0,0 +1,19 @@
+import praw
+import random
+
+
+reddit = praw.Reddit(
+    client_id = 'YYMdlwpb-5JHBg',
+    client_secret = 'CJQmIaS3eA16qBe6JguS0HBLv9I',
+    username = 'nicki_bot',
+    password = 'nickiistweet',
+    user_agent = 'ainthetweet'
+)
+subreddit = reddit.subreddit('aww')
+niki = ['https://imgur.com/a/8NOy3TK', 'https://imgur.com/a/WCJhNZJ', 'https://imgur.com/a/VX11gy7', 'https://imgur.com/a/sGUimwm', 'https://imgur.com/a/zXIkDHJ', 'https://imgur.com/a/84JLrWX']
+new_posts = subreddit.new(limit=10)
+
+for comment in subreddit.stream.comments():
+    if 'nicki' in comment.body:
+        print('we got a live one')
+        comment.reply(str('You know who else is named nicki? ----> ' + random.choice(niki) + ' aint he tweet!'))
---
+++ Python Scripts/simple_nn.py
@@ -0,0 +1,6 @@
+l1 = []
+l2 = []
+l3 = []
+
+for x in range(4):
+    print(x)
