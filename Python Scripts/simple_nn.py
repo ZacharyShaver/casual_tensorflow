2bdbaaa4f0f0689d0617906bd0951334fccf903e
Zak Shaver <zshaver1@uncc.edu>
2018-11-08 22:20:00 -0500

    Made from scratch neural net (no bias just to try and make the DS)

--- Python Scripts/simple_nn.py
+++ Python Scripts/simple_nn.py
@@ -1,6 +1,53 @@
-l1 = []
-l2 = []
-l3 = []
+import numpy
+import math
 
-for x in range(4):
-    print(x)
+
+
+class Net():
+    def __init__(self, lstruc):
+        self.lstruc = lstruc
+        self.net = [] # container with 3 slots to hold the struc with nets that hold nodes that have connections 
+
+
+    def create_net(self):
+        for i , Nnodes in enumerate(self.lstruc):
+            self.net.append(Lay(Nnodes, i))
+
+    def print_net(self):
+        for layer in self.net:
+            layer.print_size()
+            layer.print_layer()
+
+class Lay:
+    def __init__(self, Nnodes, lnum):
+        self.Nnodes = Nnodes #number of nodes in this layer
+        self.lnum = lnum
+        self.nodes = []
+        for i in range(0, Nnodes):
+            self.nodes.append(Node(lnum, i))
+
+    def print_size(self):
+        print(self.lnum, self.Nnodes)
+
+    def print_layer(self):
+        for node in self.nodes:
+            #print(node.)
+            pass
+    
+class Node:
+    def __init__(self, lnum, nnum):
+        self.lnum = lnum
+        self.nnum = nnum    
+        print("NODE layer ", lnum, " node ", nnum)
+
+    
+class connection:
+    def __init__(self, weight, from_whome, to_whomst):
+        self.weight = weight
+        self.from_whome = from_whome
+        self.to_whomst = to_whomst
+    
+net = Net([2,3,1])
+
+net.create_net()
+net.print_net()
