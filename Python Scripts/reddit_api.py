7b5e8c9cca3ce01f429c8478704e5ce96db48c41
Zak Shaver <zshaver1@uncc.edu>
2018-11-08 21:03:44 -0500

    major security update

--- Python Scripts/reddit_api.py
+++ Python Scripts/reddit_api.py
@@ -1,11 +1,11 @@
 import praw
 import random
 
-
+random.seed()
 reddit = praw.Reddit(
     client_id = 'YYMdlwpb-5JHBg',
     client_secret = 'CJQmIaS3eA16qBe6JguS0HBLv9I',
-    username = 'nicki_bot',
+    username = 'nicki_bot',         #trash account for testing take it if you want it
     password = 'nickiistweet',
     user_agent = 'ainthetweet'
 )
@@ -14,6 +14,6 @@ niki = ['https://imgur.com/a/8NOy3TK', 'https://imgur.com/a/WCJhNZJ', 'https://i
 new_posts = subreddit.new(limit=10)
 
 for comment in subreddit.stream.comments():
-    if 'nicki' in comment.body:
+    if 'nicki' in comment.body and '---->' not in comment.body:
         print('we got a live one')
         comment.reply(str('You know who else is named nicki? ----> ' + random.choice(niki) + ' aint he tweet!'))
